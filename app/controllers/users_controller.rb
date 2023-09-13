7243a30eea56db72b0f1a1dc9e4889a2f27a6243
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-13 07:37:13 +0000

    9/13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
      @user = User.find(params[:id])
-     @books = @user.books
+  end
+
+  def index
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get 'home/about' => 'homes#about'
 
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
