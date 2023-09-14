0cbd57b8711352a2e0f702156ce61880ee940116
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-14 06:17:05 +0000

    9/14

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,16 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def index
+    @book = Book.new
+    @user = User.find(current_user.id)
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -11,4 +18,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
   def show
-     @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    # @books = @user.books
   end
 
   def index
+     @user = User.find(params[:id])
+     @books = @user.books
   end
 
   def edit
