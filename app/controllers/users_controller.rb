0670adad49c42c44b29ef5fdb33e3c2d29b8e393
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-16 11:58:58 +0000

    9/16]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
   def index
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     @users = User.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    user =  User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(user)
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
