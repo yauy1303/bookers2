16336e6f55499027150a9278fede219662bd6722
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-09-18 02:37:32 +0000

    9/18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,15 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
+    @user = User.find(current_user.id)
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
+    else
+      render :index
     end
   end
 
@@ -26,10 +30,13 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.new(book_params)
     book = Book.find(params[:id])
     if book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(book.id)
+    else
+      render :edit
     end
   end
 
